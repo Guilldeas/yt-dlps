b4a13c0cc998c232f42ac54a43fd23a43b478be8
guilldeas <guilldeas@gmail.com>
2026-07-29 22:55:26 +0200

    First working state

---
+++ Custom_Playlists/download_playlists.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+#----- Variables
+declare -A list_info_arr
+
+#----- Functions
+get_config(){
+        local index
+
+        # Read list genres and urls from json into a temporary file
+        jq -r 'keys_unsorted[]' Utils/lists_info_test.json > Utils/tmplists.txt
+        jq -r 'values[]' Utils/lists_info_test.json > Utils/tmpurls.txt
+
+        # Create array from tmp file
+        mapfile -t genres < Utils/tmplists.txt
+        mapfile -t urls < Utils/tmpurls.txt
+
+        rm Utils/tmplists.txt
+        rm Utils/tmpurls.txt
+        
+        # Write data into array
+        for ((index=0; index<"${#genres[@]}"; index++)); do
+                list_info_arr["${genres[index]}"]="${urls[index]}"
+        done
+}
+
+
+build_folder_struct(){
+	# Find whether expected directory structure is present
+	for genre in "${!list_info_arr[@]}"; do
+		if [[ -d "$genre" ]]; then
+        		continue
+
+		# If not then build it
+		else
+	        	mkdir -p "$genre" \
+			&& echo "Created directory $genre"
+		fi
+	done
+}
+
+
+download_playlists() {
+    local genre
+    local url
+
+    for genre in "${!list_info_arr[@]}"; do
+        url="${list_info_arr[$genre]}"
+
+        yt-dlp \
+            --cookies-from-browser firefox \
+            -x \
+            --audio-format mp3 \
+            --add-metadata \
+            --embed-thumbnail \
+            --convert-thumbnails jpg \
+            --parse-metadata "playlist_index:%(track_number)s" \
+            -o "$genre/%(playlist_index)03d - %(title)s.%(ext)s" \
+            --download-archive "$genre/archive.txt" \
+	    --fragment-retries infinite \
+            --retry-sleep fragment:exp=1:30 \
+            --postprocessor-args "-metadata genre=$genre" \
+            "$url"
+    done
+}
+
+# ----- Main code
+get_config && build_folder_struct && download_playlists
