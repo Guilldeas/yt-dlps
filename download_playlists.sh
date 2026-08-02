00c52f7a5f63337e78dd344b7e0e81772fbeabf1
guilldeas <guilldeas@gmail.com>
2026-08-02 21:20:58 +0200

    merged parser and downloader into one script

--- download_playlists.sh
+++ download_playlists.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-#----- Variables
+
+#----- Variables -------------------------------------------------------------
 declare -A list_info_arr
 
-#----- Functions
+#----- Functions -------------------------------------------------------------
+
 get_config(){
         local index
 
@@ -64,5 +66,46 @@ download_playlists() {
     done
 }
 
-# ----- Main code
-get_config && build_folder_struct && download_playlists
+
+parser(){
+	local string="$1"
+	local charnum="$2"
+	echo "${string:"$charnum":1}"
+}
+
+downloader(){
+	get_config && build_folder_struct && download_playlists
+}
+
+
+main(){
+
+	# Capture other scripts stdout and parse it
+	local downloading_str="[download] Downloading item "
+	local downloaded_str="has already been recorded in the archive"
+	local new_playlist_str="[download] Downloading playlist: "
+
+	# Run the download and pipe it's stdout and stderr to the read command
+	downloader 2>&1 |
+
+	# Read one line from standard input while treating \ as a character 
+	while read -r output; do
+		
+		#Parse output line
+		case "$output" in
+		 *"$downloading_str"*) 
+			songnum=$(parser "$output" 28)
+			echo "downloading song number:$songnum" 
+		;;	
+		*"$downloaded_str"*)
+			((songnum+=1))
+			echo "downloading song number:$songnum" 
+		;;
+		*"$new_playlist_str"*)
+			songnum=0
+		esac
+	done
+}
+
+# ----- Main code -------------------------------------------------------------
+main
